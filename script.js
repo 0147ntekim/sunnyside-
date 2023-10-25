cd7bc45ec6be17bc37c331ae500e87fc79957160
0147ntekim <147ntekim@gmail.com>
2023-10-25 17:49:41 +0100

    commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const closeNav = document.querySelector('.close-nav')
 const hamburger = document.querySelector('.hamburger')
-const navMenu=document.querySelector('.nav-menu')
+const navLinks=document.querySelector('.nav-links')
+const logo = document.querySelector('.logo')
 
 
 hamburger.addEventListener('click', openNav)
@@ -9,11 +10,13 @@ closeNav.addEventListener('click', closeNavBar)
 function openNav(){
     closeNav.classList.add('open-nav')
     hamburger.classList.add('close-hamburger')
-    navMenu.classList.add('open-mobile-nav')
+    navLinks.classList.add('open-mobile-nav')
+    logo.classList.add('open-logo-nav-link')
 }
 
 function closeNavBar(){
     hamburger.classList.remove('close-hamburger')
     closeNav.classList.remove('open-nav')
-    navMenu.classList.remove('open-mobile-nav')
+    navLinks.classList.remove('open-mobile-nav')
+    logo.classList.remove('open-logo-nav-link')
 }
